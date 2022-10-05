1ccd1be949ae7590d3068b94c2241abc5caa8cb6
Hossy <Hossy@users.noreply.github.com>
2022-10-05 06:07:08 -0500

    testing SetKeepAlive

--- logstash.go
+++ logstash.go
@@ -1,15 +1,19 @@
 package logstash
 
 import (
+	// "crypto/tls"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"reflect"
 	"regexp"
 	"strings"
 	"time"
+	"unsafe"
 
 	docker "github.com/fsouza/go-dockerclient"
 	"github.com/gliderlabs/logspout/router"
@@ -39,6 +43,13 @@ func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
 		conn, err := transport.Dial(route.Address, route.Options)
 
 		if err == nil {
+
+			log.Println("logstash: conn:")
+			fmt.Printf("%+v\n", conn)
+			connOut, _ := json.Marshal(conn)
+			fmt.Println(string(connOut))
+			log.Println("logstash: -----")
+
 			return &LogstashAdapter{
 				route:          route,
 				conn:           conn,
@@ -199,9 +210,101 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 		// To work with tls and tcp transports via json_lines codec
 		js = append(js, byte('\n'))
 
+		log.Println("logstash: logging [start] -> ", m.Data)
+
 		for {
+
 			_, err := a.conn.Write(js)
 
+			log.Println("logstash: a:")
+			fmt.Printf("%+v\n", a)
+			if aOut, err := json.Marshal(a); err != nil {
+				log.Println("logstash: Failed to marshal a")
+			} else {
+				fmt.Println(string(aOut))
+			}
+
+			log.Println("logstash: a.conn:")
+			fmt.Printf("%+v\n", a.conn)
+			if connOut, err := json.Marshal(a.conn); err != nil {
+				log.Println("logstash: Failed to marshal a.conn")
+			} else {
+				fmt.Println(string(connOut))
+			}
+
+			log.Println("logstash: a reflection: ", reflect.ValueOf(a).Kind())
+			log.Println("logstash: *a reflection: ", reflect.ValueOf(*a).Kind())
+			log.Println("logstash: &*a Elem reflection: ", reflect.ValueOf(&*a).Elem())
+			ac := a.conn
+			log.Println("logstash: Kind a.conn reflection: ", reflect.ValueOf(ac).Kind())
+			log.Println("logstash: Type a.conn reflection: ", reflect.ValueOf(ac).Type())
+			log.Println("logstash: Kind & a.conn reflection: ", reflect.ValueOf(&ac).Kind())
+			log.Println("logstash: Elem a.conn reflection: ", reflect.ValueOf(ac).Elem())
+			//log.Println("logstash: Elem.conn.Elem a.conn reflection: ", reflect.ValueOf(ac).Elem().conn.Elem())
+			log.Println("logstash: Elem.Type a.conn reflection: ", reflect.ValueOf(ac).Elem().Type())
+			log.Println("logstash: Elem.Kind a.conn reflection: ", reflect.ValueOf(ac).Elem().Kind())
+			log.Println("logstash: Elem.NumField a.conn reflection: ", reflect.ValueOf(ac).Elem().NumField())
+			
+			// log.Println("create rs")
+			// rs := reflect.ValueOf(ac).Elem()
+			// log.Println("create rs2")
+			// rs2 := reflect.New(rs.Type()).Elem()
+			// log.Println("set rs2")
+			// rs2.Set(rs)
+			// log.Println("create rf")
+			// rf := rs2.Field(0)
+			// log.Println("set rf")
+			// rf = reflect.NewAt(rf.Type(), unsafe.Pointer(rf.UnsafeAddr())).Elem()			
+			// log.Println("magic done")
+
+			//ac := a.conn
+			log.Println("create s")
+			s := reflect.ValueOf(ac).Elem()
+			log.Println("create typeOfT")
+			typeOfT := s.Type()
+			
+			log.Println("get NumField")
+			for i := 0; i < s.NumField(); i++ {
+				log.Println("get Field")
+				f := s.Field(i)
+				log.Println("fmt.Printf")
+				fmt.Printf("%d: %s %s\n", i,
+					typeOfT.Field(i).Name, f.Type())
+				log.Println("reflect.NewAt")
+				f = reflect.NewAt(f.Type(), unsafe.Pointer(f.UnsafeAddr())).Elem()			
+				log.Println("fmt.Printf")
+				fmt.Printf("%d: %s %s = %v\n", i,
+					typeOfT.Field(i).Name, f.Type(), f.Interface())
+				log.Println("loop end")
+			}			
+
+			log.Println("get NumMethod: ", typeOfT.NumMethod())
+			for i := 0; i < s.NumMethod(); i++ {
+				log.Println("get Method")
+				m := typeOfT.Method(i)
+				//log.Println("reflect.NewAt")
+				//f = reflect.NewAt(f.Type(), unsafe.Pointer(f.UnsafeAddr())).Elem()			
+				log.Println("fmt.Printf")
+				fmt.Printf("%d: %s %s\n", i,
+					m.Name, m.Type)
+				log.Println("loop end")
+			}			
+
+			// log.Println("logstash: a.conn reflection:")
+			// ac := a.conn
+			// s := reflect.ValueOf(&ac).Elem()
+			// typeOfT := s.Type()
+			// 
+			// for i := 0; i < s.NumField(); i++ {
+			// 	f := s.Field(i)
+			// 	fmt.Printf("%d: %s %s = %v\n", i,
+			// 		typeOfT.Field(i).Name, f.Type(), f.Interface())
+			// }			
+
+			log.Println("logstash: -----")
+			//log.Println("logstash: logging [isConnected] -> ", a.conn.isConnected)
+			log.Println("logstash: logging [end] -> ", m.Data)
+			log.Println("logstash: logging Write -> ", err)
 			if err == nil {
 				break
 			}
